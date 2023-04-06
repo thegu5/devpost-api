58a68e70486f9d11aa49f56c0ddc83251392244e
Dr. Gold <james.h.parmelee@gmail.com>
2023-04-06 00:03:05 -0400

    very minor update to hackathon.js

--- src/routes/hackathon/hackathon.js
+++ src/routes/hackathon/hackathon.js
@@ -10,7 +10,7 @@ module.exports = async function (fastify, options) {
         shortDescription: 'div#introduction .row .content > h3',
         description: '#challenge-description | trim',
         banner: 'header#challenge-header > img@src | img',
-        // bannerColor: "style:contains('#challenge-header') | bghex",
+        bannerColor: "style:contains('#challenge-header')",
         time: '#date-info-tag > time',
         location: 'table.is-marginless > tbody > tr > td > div > div:nth-child(2) | trim',
         address: 'table.is-marginless > tbody > tr > td > div > div:nth-child(2) > a@href | address',
@@ -21,7 +21,7 @@ module.exports = async function (fastify, options) {
           'div.info-with-icon:nth-child(2) > div:nth-child(2) > a | trim'
         ],
         requirementText: '#challenge-requirements > div:nth-child(2) | trim',
-        requirements: 'string',
+        requirements: '#eligibility-list | trim',
         prizes: x('.prize', [
           {
             name: '.prize-title | trim',
@@ -36,9 +36,6 @@ module.exports = async function (fastify, options) {
         ]),
         judgingCriteria: '#judging-criteria .no-bullet | trim'
       })
-    if (data.address === undefined) {
-      data.address = null
-    }
     return data
   })
 }
