b627f3e1386e97e6c283d2ae2e1cdfec5bf640c5
Dr. Gold <james.h.parmelee@gmail.com>
2023-04-06 01:28:45 -0400

    add software/likes.js
    +likes.js

---
+++ src/routes/software/likes.js
@@ -0,0 +1,29 @@
+const x = require('../../xray.js')
+
+module.exports = async function (fastify, options) {
+    fastify.get('/:software/likes', async (request, reply) => {
+        const software = request.params.software
+        const baseUrl = `https://devpost.com/software/${software}/likes?page=:page`
+        const temp = []
+        let page = 1
+        let hasNextPage = true
+
+        while (hasNextPage) {
+            const url = baseUrl.replace(':page', page)
+            const data = await x(url, {
+                likes: x('a.like-row', [
+                    {
+                      name: ".content | trim",
+                      username: "@href | slug",
+                      url: "@href",
+                      avatar: "img@src"
+                    }
+                  ])
+            })
+            temp.push(...data.likes)
+            hasNextPage = data.likes.length > 0
+            page++
+        }
+        return {temp}
+    });
+  }
